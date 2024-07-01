02dc3f58b01ea604cdbb7c3843a4653e8f609a53
Nikkitha Venkatesan <126761863+nikkitha0806@users.noreply.github.com>
2024-07-01 08:24:30 +0100

    Time: 57 ms (19.29%), Space: 16.8 MB (95.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -4,14 +4,25 @@ class Solution:
         # check if the length of both the string are same:
         if len(s) != len(t):
             return False
+
+        # without using counter:
+        countS, countT = {}, {}
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0) # to avoid the key error, we are adding 0 as default
         
-        else:
-            hashmap = dict(Counter(s))
-            for i in range(len(t)):
-                if t[i] not in hashmap or hashmap[t[i]]==0:
-                   return False
-                else:
-                    if t[i] in hashmap and hashmap[t[i]] > 0:
-                        hashmap[t[i]] -=1
-            return True
+        for c in countS:
+            if countS[c] !=countT.get(c, 0):
+                return False
+        return True
+        
+        # else:
+        #     hashmap = dict(Counter(s))
+        #     for i in range(len(t)):
+        #         if t[i] not in hashmap or hashmap[t[i]]==0:
+        #            return False
+        #         else:
+        #             if t[i] in hashmap and hashmap[t[i]] > 0:
+        #                 hashmap[t[i]] -=1
+        #     return True
         
