e3079ad4d571be510f44a853c9d4d317cbf8bb6e
Nikkitha Venkatesan <126761863+nikkitha0806@users.noreply.github.com>
2024-07-03 15:41:08 +0200

    Delete 1415-students-and-examinations directory

--- 1415-students-and-examinations/1415-students-and-examinations.sql
+++
@@ -1,8 +0,0 @@
-# Write your MySQL query statement below
-select st.student_id, st.student_name, sub.subject_name, COUNT(ex.student_id) as attended_exams
-from Students as st
-join Subjects sub
-left join Examinations as ex
-on st.student_id = ex.student_id and sub.subject_name = ex.subject_name
-group by st.student_id, st.student_name, sub.subject_name
-order by st.student_id, sub.subject_name
